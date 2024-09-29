28f2269db8b48ba05b0c4a119c4c483df7d69c0b
Enzo <e.gallo1024@gmail.com>
2024-09-29 19:10:28 +0200

    first commit

---
+++ ZozoApp/ZozoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZozoAppApp.swift
+//  ZozoApp
+//
+//  Created by Enzo Perso on 29/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct ZozoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ZozoAppTests/ZozoAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  ZozoAppTests.swift
+//  ZozoAppTests
+//
+//  Created by Enzo Perso on 29/09/2024.
+//
+
+import Testing
+@testable import ZozoApp
+
+struct ZozoAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
